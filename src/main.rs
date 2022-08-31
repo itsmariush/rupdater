9b4af0df40c3d8d9529763c9ce9314843262469c
sparer <sparer@tower.xyz>
2022-08-31 17:35:34 +0200

    Download file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use std::env;
+#![allow(dead_code)]
+#![allow(unused_variables)]
 
+extern crate confy;
+extern crate reqwest;
+
+use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::io;
 use serde::{Serialize, Deserialize};
+
 
 #[derive(Debug, Serialize, Deserialize)]
 struct RuConfig {
@@ -16,17 +25,31 @@ struct RuConfig {
 
 impl Default for RuConfig {
     fn default() -> Self {
-        RuConfig {
+        Self {
             zipurl : "http://localhost:8000/update.zip".to_string(),
             sigurl : "http://localhost:8000/update.zip.sig".to_string(),
             tmppath: "/tmp/rupdater".to_string(),
             destpath: "./dest".to_string(),
             user: "sparer".to_string(),
             chmod: "0755".to_string(),
-            pre_cmd: "None".to_string(),
-            post_cmd: "None".to_string(),
+            pre_cmd: "".to_string(),
+            post_cmd: "".to_string(),
         }
     }
+}
+
+fn download_file(url : &str, dest : &str) {
+    let mut response = reqwest::blocking::get(url).expect("Failed connect to server");
+    let filename = response
+                    .url()
+                    .path_segments()
+                    .and_then(|segments| segments.last())
+                    .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                    .unwrap_or("tmp.bin");
+    let out_path = Path::new(dest).join(filename);
+    println!("{}", out_path.to_str().unwrap());
+    let mut out_file = File::create(out_path).expect("Failed to create output file");
+    io::copy(&mut response, &mut out_file).expect("failed to copy content of downloaded file");
 }
 
 fn main() -> Result<(), confy::ConfyError> {
@@ -38,6 +61,8 @@ fn main() -> Result<(), confy::ConfyError> {
     }
     
     let conf : RuConfig = confy::load("rupdater")?;
-    dbg!(conf);
+    dbg!(&conf);
+    println!("Download zip file from {}", &conf.zipurl);
+    download_file(&conf.zipurl, &conf.tmppath);
     Ok(())
 }
