6a955f80860d080c268af624ecb5a9f3bece7f26
sparer <sparer@tower.xyz>
2022-08-31 11:28:23 +0200

    Added confy and default config file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 use std::env;
 
-fn main() {
-    let args : Vec<String> = env::args().collect();
-    
-    let filepath: &str = &args[1];
+use serde::{Serialize, Deserialize};
 
-    println!("Arg1: {}", filepath);
-    dbg!(args);
-    println!("Hello, world!");
+#[derive(Debug, Serialize, Deserialize)]
+struct RuConfig {
+    zipurl : String,
+    sigurl : String,
+    tmppath: String,
+    destpath: String,
+    user: String,
+    chmod: String,
+    pre_cmd: String,
+    post_cmd: String,
+}
+
+impl Default for RuConfig {
+    fn default() -> Self {
+        RuConfig {
+            zipurl : "http://localhost:8000/update.zip".to_string(),
+            sigurl : "http://localhost:8000/update.zip.sig".to_string(),
+            tmppath: "/tmp/rupdater".to_string(),
+            destpath: "./dest".to_string(),
+            user: "sparer".to_string(),
+            chmod: "0755".to_string(),
+            pre_cmd: "None".to_string(),
+            post_cmd: "None".to_string(),
+        }
+    }
+}
+
+fn main() -> Result<(), confy::ConfyError> {
+    println!("Starting rUpdater...");
+    let args : Vec<String> = env::args().collect();
+
+    if args.len() == 0 {
+
+    }
+    
+    let conf : RuConfig = confy::load("rupdater")?;
+    dbg!(conf);
+    Ok(())
 }
